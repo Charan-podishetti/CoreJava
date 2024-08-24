eb377d7925603197f363347241f9b13e02f4e2a1
charan <podishettic28280@gmail.com>
2024-08-24 22:35:07 +0530

    application 5 removed

--- Application/Application5/InvalidAgeException.java
+++
@@ -1,9 +0,0 @@
-package com.evergent.corejava.charan.Application5;
-
-
-class InvalidAgeException extends Exception {
-	public InvalidAgeException(String message) {
-		super(message);
-	}
-
-}
--- Application/Application5/LibraryUser.java
+++
@@ -1,6 +0,0 @@
-package com.evergent.corejava.charan.Application5;
-
-public interface LibraryUser {
-void registerAccount(int age) throws InvalidAgeException;
-void requestBook(String btype) throws InvalidBookTypeException;
-}
