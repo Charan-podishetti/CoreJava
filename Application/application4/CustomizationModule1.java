36961495055f3cfadc64cfc5aeb93082ecbfeafd
charan <podishettic28280@gmail.com>
2024-08-26 09:22:45 +0530

    Application deleted

--- Application/application4/CustomizationModule1.java
+++
@@ -1,6 +0,0 @@
-package com.evergent.corejava.charan.application4;
-public interface CustomizationModule1 extends Module1{
-    void chooseDesign();
-    void chooseColor();
-    
-}
--- Application/application4/ReportModule.java
+++
@@ -1,8 +0,0 @@
-package com.evergent.corejava.charan.application4;
-
-public interface ReportModule extends Module1 {
-    void displayInvoice();
-    void displaySummary();
-    
-}
-
