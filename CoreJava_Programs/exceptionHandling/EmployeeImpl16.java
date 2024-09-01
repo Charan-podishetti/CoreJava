f298d4e53dd82da132151088d49b4b57ac87fff2
charan <podishettic28280@gmail.com>
2024-09-01 18:21:59 +0530

    coreJava programs was deleted

--- CoreJava_Programs/exceptionHandling/EmployeeImpl16.java
+++
@@ -1,7 +0,0 @@
-package com.evergent.corejava.exceptionHandling;
-
-public class EmployeeImpl16 {
-
-		
-
-}
--- CoreJava_Programs/interface1/NewBook.java
+++
@@ -1,7 +0,0 @@
-package com.evergent.corejava.interface1;
-//one interface can extends other interface.
-public interface NewBook extends MyDataInfo{
-public void addNewBook();
-public void bookPrice(); //we have the same method in Book interface. Therefore we can say that
-//interface supports multiple inheritance.
-}
