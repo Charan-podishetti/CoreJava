57fbc8e433490a02dec165aff13e445f6a4d6a3f
charan <podishettic28280@gmail.com>
2024-09-18 12:07:16 +0530

    Student application was added

---
+++ LayerWise Architecture/StudentApplication/StudentBean.java
@@ -0,0 +1,37 @@
+package com.evergent.StudentApplication;
+import java.io.Serializable;
+
+public class StudentBean implements Serializable {
+	private String name;
+	private float score;
+	private String grade;
+	private int studentId;
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getScore() {
+		return score;
+	}
+	public void setScore(float score) {
+		this.score = score;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	
+	
+	
+	
+}
