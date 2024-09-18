ee920a3fec457f34cd6ce520211f3b32a22e7105
charan <podishettic28280@gmail.com>
2024-09-18 09:01:17 +0530

    Application5 was deleted

--- CoreJAVA_Applications/Application5/InvalidBookTypeException.java
+++
@@ -1,9 +0,0 @@
-package com.evergent.corejava.charan.Application5;
-
-
-class InvalidBookTypeException extends Exception {
-	public InvalidBookTypeException(String message) {
-		super(message);
-	}
-
-}
